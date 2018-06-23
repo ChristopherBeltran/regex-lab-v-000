35517e4ef7ef4ff16a8a000fbe50ba90ca2c12f6
Christopher Beltran <christod.beltran@gmail.com>
2018-06-23 22:29:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,19 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/\b[AEIOUaeiou]/)
+  return true 
+else
+  return false
+end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+text.scan(/un\w+ing/)
 
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b{5}\s/)
+return
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
